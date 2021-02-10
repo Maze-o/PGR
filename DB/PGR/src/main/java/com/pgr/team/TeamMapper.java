71d3684f14841c9fd25686832caf640ac4c36834
pakafans <75054298+sh020308@users.noreply.github.com>
2021-02-10 12:46:53 +0900

    0210 team stat insert 추가
    다음할 작업 팀통계 update

---
+++ DB/PGR/src/main/java/com/pgr/team/TeamMapper.java
@@ -0,0 +1,13 @@
+package com.pgr.team;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.pgr.model.TeamEntity;
+
+@Mapper
+public interface TeamMapper {
+	int insTeam(List<TeamEntity> p);
+	List<TeamEntity> selTeamList();
+}

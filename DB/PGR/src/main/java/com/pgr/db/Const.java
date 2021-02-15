30fa3bcf7ab92a1e41fd059667a1e06a2b6c4eb2
pakafans <75054298+sh020308@users.noreply.github.com>
2021-02-15 17:19:04 +0900

    a

--- DB/PGR/src/main/java/com/pgr/db/Const.java
+++
@@ -1,7 +0,0 @@
-package com.pgr.db;
-
-// API주소
-public class Const {
-	public static final String RECENT_MATCHES = "http://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?limit=5";
-	public static final String TEAMS_STAT = "http://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/teams";
-}

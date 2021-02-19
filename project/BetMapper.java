c0a4c4a025cdad2d50fa1f4e56d02a42f370833b
tuktarr <68638065+tuktarr@users.noreply.github.com>
2021-02-19 20:54:19 +0900

    betting 후 update 완료

---
+++ project/BetMapper.java
@@ -0,0 +1,17 @@
+package com.pgr.bet;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.pgr.model.BetDTO;
+import com.pgr.model.BetEntity;
+import com.pgr.model.RecentEntity;
+
+@Mapper
+public interface BetMapper {
+	int insBet(BetEntity p);
+	List<BetEntity> selBetList(RecentEntity p);
+	int updBetSuccess(BetEntity p);
+	int updBetuser(BetEntity p);
+}

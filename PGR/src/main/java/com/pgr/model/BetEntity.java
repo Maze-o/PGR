2b93155ff34bb6cc32e146657659fce52ee17e03
DESKTOP-ISVFPRU\Administrator <kyoshin_95@naver.com>
2021-02-22 16:07:15 +0900

    bet
    BET 모델 + 패키지 추가

---
+++ PGR/src/main/java/com/pgr/model/BetEntity.java
@@ -0,0 +1,83 @@
+package com.pgr.model;
+
+public class BetEntity {
+	private int userPk;
+	private int property;
+	private int id;
+	private int myProperty;
+	private int team;
+	private int bResult;
+	private int success;
+	private int win;
+	private int lose;
+	private int draw;
+	private int cTeam;
+	public int getUserPk() {
+		return userPk;
+	}
+	public void setUserPk(int userPk) {
+		this.userPk = userPk;
+	}
+	public int getProperty() {
+		return property;
+	}
+	public void setProperty(int property) {
+		this.property = property;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMyProperty() {
+		return myProperty;
+	}
+	public void setMyProperty(int myProperty) {
+		this.myProperty = myProperty;
+	}
+	public int getTeam() {
+		return team;
+	}
+	public void setTeam(int team) {
+		this.team = team;
+	}
+	public int getbResult() {
+		return bResult;
+	}
+	public void setbResult(int bResult) {
+		this.bResult = bResult;
+	}
+	public int getSuccess() {
+		return success;
+	}
+	public void setSuccess(int success) {
+		this.success = success;
+	}
+	public int getWin() {
+		return win;
+	}
+	public void setWin(int win) {
+		this.win = win;
+	}
+	public int getLose() {
+		return lose;
+	}
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+	public int getDraw() {
+		return draw;
+	}
+	public void setDraw(int draw) {
+		this.draw = draw;
+	}
+	public int getcTeam() {
+		return cTeam;
+	}
+	public void setcTeam(int cTeam) {
+		this.cTeam = cTeam;
+	}
+	
+	
+}

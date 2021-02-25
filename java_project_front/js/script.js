270f632734888b15992423670d14b940c6006aaa
Matic <64820550+msc010312@users.noreply.github.com>
2021-02-25 16:17:30 +0900

    2021 02 25

--- java_project_front/js/script.js
+++ java_project_front/js/script.js
@@ -7,6 +7,38 @@ window.onload = function() {
 
     // 새로고침시 스크롤 상단위치
     history.scrollRestoration = "manual"
+
+    // news slide
+    new Swiper('.sw_news', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            prevEl: '.news_slide .sw_prev',
+            nextEl: '.news_slide .sw_next',
+        },
+        pagination: {
+            el: '.news_slide .sw_pg',
+            type: 'bullets',
+            // clickable: true,
+        },
+    });
+
+     // swiper 일시멈춤/재생 기능
+    const pause = document.getElementByClassName('.sw_pause')
+    pause.click(function() {
+        var state = $(this).hasClass('bt_play');
+        if (state == false) {
+            sw_24.autoplay.stop();
+            $(this).addClass('bt_play');
+        } else {
+            sw_24.autoplay.start();
+            $(this).removeClass('bt_play');
+        }
+    });
+
 }
 
 // $(document).ready(function() {
